d3250cfb348afb98c6e930fd6b39fe680d6ba69d
Carlos Lostao <hastro1990@gmail.com>
2024-09-26 17:55:33 +0200

    feat: icon depends on object type

--- frontend/src/components/Files/UploadingObjects.tsx
+++ frontend/src/components/Files/UploadingObjects.tsx
@@ -7,7 +7,7 @@ import {
   UploadStatus,
 } from "../../models/UploadedObjectMetadata";
 import { ApiService } from "../../services/api";
-import { CrossIcon, TrashIcon, X } from "lucide-react";
+import { CrossIcon, FileIcon, FolderIcon, TrashIcon, X } from "lucide-react";
 
 export const UploadingObjects = () => {
   const [uploadingObjects] = useLocalStorage<string[]>("uploading-objects", []);
@@ -85,20 +85,7 @@ const UploadingObject = ({
       <div className="border border-blue-300 rounded-lg p-4 relative">
         <div className="flex items-start mb-4">
           <div className="bg-gray-200 rounded p-2 mr-3">
-            <svg
-              className="w-6 h-6 text-gray-500"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
-              />
-            </svg>
+            {metadata.type === "file" ? <FileIcon /> : <FolderIcon />}
           </div>
           <div>
             <p className="font-semibold">{metadata.name}</p>
