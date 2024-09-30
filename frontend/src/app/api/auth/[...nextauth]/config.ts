fb0fdc5d53b6904ed1fa2002cbba9d30724a1546
Carlos Lostao <hastro1990@gmail.com>
2024-10-01 00:08:35 +0200

    update: extract config to external file

---
+++ frontend/src/app/api/auth/[...nextauth]/config.ts
@@ -0,0 +1,26 @@
+import { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: AuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_AUTH_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET as string,
+      checks: [],
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      // @ts-ignore
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -1,29 +1,5 @@
-import NextAuth, { type AuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-
-export const authOptions: AuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_AUTH_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET as string,
-      checks: [],
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, account }) {
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      // @ts-ignore
-      session.accessToken = token.accessToken;
-      return session;
-    },
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-};
+import NextAuth from "next-auth";
+import { authOptions } from "./config";
 
 const handler = NextAuth(authOptions);
 
