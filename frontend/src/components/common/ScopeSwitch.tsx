0aab0be3fb52ae9216a83c00678ad1bf9f427811
clostao <49268919+clostao@users.noreply.github.com>
2024-10-01 23:45:57 +0200

    fix: scope switch (#30)

--- frontend/src/components/common/ScopeSwitch.tsx
+++ frontend/src/components/common/ScopeSwitch.tsx
@@ -1,27 +1,31 @@
 "use client";
 
 import { Switch } from "@headlessui/react";
+import { useEffect, useState } from "react";
 import { useLocalStorage } from "usehooks-ts";
 
 export const ScopeSwitch = () => {
+  const [global, setGlobal] = useState(false);
   const [scope, setScope] = useLocalStorage<"user" | "global">(
     "search-scope",
     "global"
   );
 
+  useEffect(() => {
+    setScope(global ? "global" : "user");
+  }, [global]);
+
   return (
     <Switch
-      checked={scope === "global"}
-      onChange={() => {
-        setScope(scope === "global" ? "user" : "global");
-      }}
+      checked={global}
+      onClick={() => setGlobal(!global)}
       className={`${
-        scope === "global" ? "bg-blue-600" : "bg-gray-200"
+        global ? "bg-blue-600" : "bg-gray-200"
       } relative inline-flex h-6 w-11 items-center rounded-full transition-colors focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2`}
     >
       <span
         className={`${
-          scope === "global" ? "translate-x-6" : "translate-x-1"
+          global ? "translate-x-6" : "translate-x-1"
         } inline-block h-4 w-4 transform rounded-full bg-white transition-transform`}
       />
     </Switch>
