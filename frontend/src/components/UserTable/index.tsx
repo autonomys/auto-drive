e85db00cc132fb5db849ba8d2b3094d36f1b67f5
Carlos Lostao <hastro1990@gmail.com>
2024-10-18 14:52:07 +0200

    chore: remove button outdated

--- frontend/src/components/UserTable/index.tsx
+++ frontend/src/components/UserTable/index.tsx
@@ -1,4 +1,3 @@
-import { useCallback, useEffect, useState } from "react";
 import { UserTableRow } from "./UserTableRow";
 import { SubscriptionWithUser } from "../../models/Subscriptions";
 
@@ -7,19 +6,8 @@ export const UserSubscriptionsTable = ({
 }: {
   users: SubscriptionWithUser[];
 }) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const closeModal = useCallback(() => setIsOpen(false), []);
-  const openModal = useCallback(() => setIsOpen(true), []);
-
   return (
     <div>
-      <button
-        className="text-white bg-gray-500 hover:bg-gray-600 px-3 py-1 rounded mr-2 mb-4"
-        onClick={openModal}
-      >
-        Create API Key
-      </button>
       <div className="-my-2 sm:-mx-6 lg:-mx-8">
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
           <div className="shadow border-b border-gray-200 sm:rounded-lg">
