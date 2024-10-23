0dcfee4424694e175ec9af0ff038e19189f6e8c6
Carlos Lostao <hastro1990@gmail.com>
2024-10-23 02:16:40 +0200

    feat: added actions to metadata view

--- frontend/src/components/UploadedObjectInformation/index.tsx
+++ frontend/src/components/UploadedObjectInformation/index.tsx
@@ -3,30 +3,68 @@ import {
   OwnerRole,
   UploadedObjectMetadata,
 } from "../../models/UploadedObjectMetadata";
-import { getTypeFromMetadata } from "../../utils/file";
+import { getTypeFromMetadata, handleFileDownload } from "../../utils/file";
 import { useUserStore } from "../../states/user";
-import { useMemo } from "react";
+import { useCallback, useMemo, useState } from "react";
+import { Button } from "../common/Button";
+import { ApiService } from "../../services/api";
+import { ObjectShareModal } from "../Files/ObjectShareModal";
+import { ObjectDeleteModal } from "../Files/ObjectDeleteModal";
 
 export const UploadedObjectInformation = ({
   object,
 }: {
   object: UploadedObjectMetadata | null;
 }) => {
-  if (!object) {
-    return <Loader className="animate-spin" />;
-  }
-
+  const [shareModalCid, setShareModalCid] = useState<string | null>(null);
+  const [deleteModalCid, setDeleteModalCid] = useState<string | null>(null);
   const { user } = useUserStore();
-  const ownerHandle = object.owners.find(
+  const ownerHandle = object?.owners.find(
     (o) => o.role === OwnerRole.ADMIN
   )?.handle;
 
   const owners = useMemo(() => {
-    return object.owners.sort((a, b) => a.role.localeCompare(b.role));
-  }, [object.owners]);
+    return object?.owners.sort((a, b) => a.role.localeCompare(b.role));
+  }, [object?.owners]);
+
+  const isOwner = owners?.some(
+    (o) => o.handle === user?.handle && o.role === OwnerRole.ADMIN
+  );
+  const hasFileOwnership = object?.owners.some(
+    (o) => o.handle === user?.handle
+  );
+
+  const handleDownload = useCallback(async () => {
+    if (!object) {
+      return;
+    }
+
+    const blob = await ApiService.downloadObject(object.metadata.dataCid);
+    handleFileDownload(
+      blob,
+      object.metadata.type ?? "file",
+      object.metadata.name ?? "Unnamed"
+    );
+  }, [object?.metadata.dataCid, object?.metadata.type, object?.metadata.name]);
+
+  const handleShare = useCallback(() => {
+    setShareModalCid(object?.metadata.dataCid ?? null);
+  }, [object?.metadata.dataCid]);
+
+  const handleDelete = useCallback(() => {
+    setDeleteModalCid(object?.metadata.dataCid ?? null);
+  }, [object?.metadata.dataCid]);
 
   return (
     <div className="flex flex-col gap-4">
+      <ObjectShareModal
+        cid={shareModalCid}
+        closeModal={() => setShareModalCid(null)}
+      />
+      <ObjectDeleteModal
+        cid={deleteModalCid}
+        closeModal={() => setDeleteModalCid(null)}
+      />
       <div className="flex items-center ml-2">
         <button
           className="bg-accent p-1 rounded-full text-white hover:bg-accent/80 hover:scale-105 transition-all"
@@ -34,6 +72,27 @@ export const UploadedObjectInformation = ({
         >
           <ArrowLeft className="w-4 h-4" />
         </button>
+      </div>
+      <div className="flex gap-4">
+        <Button variant="lightAccent" onClick={handleDownload}>
+          Download
+        </Button>
+        <Button
+          variant="lightAccent"
+          className="disabled:hidden text-sm"
+          onClick={handleShare}
+          disabled={!isOwner}
+        >
+          Share
+        </Button>
+        <Button
+          variant="danger"
+          className="disabled:hidden text-sm"
+          onClick={handleDelete}
+          disabled={!hasFileOwnership}
+        >
+          Delete
+        </Button>
       </div>
       <span className="text-xl font-semibold ml-2">Metadata</span>
       <div className="grid grid-cols-2 gap-y-2 gap-x-4 text-primary font-medium bg-gray-50 p-4 rounded-lg">
@@ -107,7 +166,7 @@ export const UploadedObjectInformation = ({
       </div>
       <span className="text-xl font-semibold ml-2">Owners</span>
       <div className="grid grid-cols-2 gap-y-2 gap-x-4 text-primary font-medium bg-gray-50 p-4 rounded-lg">
-        {owners.map((o) => (
+        {owners?.map((o) => (
           <div className="flex">
             <span>{o.role.charAt(0).toUpperCase() + o.role.slice(1)}</span>
             <span className="ml-[4px]">{o.handle}</span>
