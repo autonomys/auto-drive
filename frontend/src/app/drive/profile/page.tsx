cb1dcffff90785c0b327dc36588a0665b1b37e13
Carlos Lostao <hastro1990@gmail.com>
2024-11-01 14:41:49 +0100

    feat: logout button

--- frontend/src/app/drive/profile/page.tsx
+++ frontend/src/app/drive/profile/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { signOut } from "next-auth/react";
 import { useEffect, useState, useCallback } from "react";
 import { ApiService } from "../../../services/api";
 import { ApiKeyWithoutSecret } from "../../../models/ApiKey";
@@ -7,7 +8,7 @@ import { ApiKeysTable } from "../../../components/ApiKeysTable";
 import { Button } from "../../../components/common/Button";
 import { DefaultPasswordModal } from "../../../components/DefaultPasswordModal";
 import { useUserStore } from "../../../states/user";
-import { Loader } from "lucide-react";
+import { Loader, LogOut } from "lucide-react";
 import toast from "react-hot-toast";
 
 export default function Page() {
@@ -45,7 +46,7 @@ export default function Page() {
       <div className="flex flex-col gap-2 p-2">
         <ApiKeysTable apiKeys={apiKeys} />
       </div>
-      <div className="flex flex-col gap-2 p-2">
+      <div className="flex flex-col gap-4 p-2">
         <span className="text-2xl font-bold mb-4">My Account</span>
         <div className="flex gap-2">
           <span>
@@ -64,6 +65,18 @@ export default function Page() {
               onClick={copyPublicId}
             >
               Copy Public ID
+            </Button>
+          </span>
+        </div>
+        <div className="flex gap-2">
+          <span>
+            <Button
+              variant="lightDanger"
+              className="flex text-sm items-center gap-2"
+              onClick={() => signOut()}
+            >
+              Log out
+              <LogOut className="w-4 h-4" />
             </Button>
           </span>
         </div>
