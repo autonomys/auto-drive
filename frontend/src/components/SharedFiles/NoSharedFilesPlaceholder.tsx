759ddbcf38291373aabd69f7200161e4d0168765
Carlos Lostao <hastro1990@gmail.com>
2024-11-05 23:28:57 +0100

    update: some copy

--- frontend/src/components/SharedFiles/NoSharedFilesPlaceholder.tsx
+++ frontend/src/components/SharedFiles/NoSharedFilesPlaceholder.tsx
@@ -2,10 +2,11 @@ export const NoSharedFilesPlaceholder = () => {
   return (
     <div className="max-w-2xl mx-auto p-6 bg-white rounded-lg">
       <h2 className="text-2xl font-bold mb-4 text-center">
-        No shared with you files yet
+        No files have been shared with you yet
       </h2>
       <p className="text-center mb-6">
-        Share files with others by clicking the share button on the top right
+        Share files with others by clicking the share button on any of your
+        files.
       </p>
     </div>
   );
--- frontend/src/components/TrashFiles/NoFilesInTrashPlaceholder.tsx
+++ frontend/src/components/TrashFiles/NoFilesInTrashPlaceholder.tsx
@@ -2,9 +2,7 @@ export const NoFilesInTrashPlaceholder = () => {
   return (
     <div className="max-w-2xl mx-auto p-6 bg-white rounded-lg">
       <h2 className="text-2xl font-bold mb-4 text-center">No files in trash</h2>
-      <p className="text-center mb-6">
-        Files you mark as deleted will appear here.
-      </p>
+      <p className="text-center mb-6">Files you remove will appear here.</p>
     </div>
   );
 };
