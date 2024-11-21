7ca122c895b59fc88f7db353bf5020cb3771be4d
Carlos Lostao <hastro1990@gmail.com>
2024-11-21 23:49:17 +0100

    chore: toaster notification placement

--- frontend/src/components/ToasterSetup/index.tsx
+++ frontend/src/components/ToasterSetup/index.tsx
@@ -1,10 +1,7 @@
 'use client';
 
 import { Toaster } from 'react-hot-toast';
-import { useMediaQuery } from 'usehooks-ts';
 
 export const ToasterSetup = () => {
-  const isMobile = useMediaQuery('(max-width: 768px)');
-
-  return <Toaster position={isMobile ? 'top-center' : 'bottom-left'} />;
+  return <Toaster position='top-center' />;
 };
