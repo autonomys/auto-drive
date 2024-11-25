ed1a1078324d25b8cf53c2a0b4004f5a0e3b9f6d
Carlos Lostao <hastro1990@gmail.com>
2024-11-25 18:29:52 +0100

    test: custom jwt auth system

--- backend/global-setup.ts
+++ backend/global-setup.ts
@@ -1,6 +1,7 @@
 import { PostgreSqlContainer } from '@testcontainers/postgresql'
 
 export default async () => {
+  process.env.JWT_SECRET = 'secret'
   const container = new PostgreSqlContainer().withExposedPorts(54320)
   const service = await container.start()
   process.env.DATABASE_URL = service.getConnectionUri()
