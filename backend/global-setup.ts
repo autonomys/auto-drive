2e0fb0d8b3c4545c13326e1cf6d1c67c31a3220e
Carlos Lostao <hastro1990@gmail.com>
2024-11-27 14:27:37 +0100

    update: extract test enviroment variables to envfile

--- backend/global-setup.ts
+++ backend/global-setup.ts
@@ -1,7 +1,8 @@
 import { PostgreSqlContainer } from '@testcontainers/postgresql'
+import { config } from 'dotenv'
 
 export default async () => {
-  process.env.JWT_SECRET = 'secret'
+  config({ path: '.env.test' })
   const container = new PostgreSqlContainer().withExposedPorts(54320)
   const service = await container.start()
   process.env.DATABASE_URL = service.getConnectionUri()
