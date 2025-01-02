2337d85e483f2aacfb8fb93f854ddb943c756904
Carlos Lostao <hastro1990@gmail.com>
2025-01-02 18:19:52 +0100

    chore: add some metadata

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,12 @@
-'use client';
-
+import { Metadata } from 'next';
 import { Home } from '../views/Home';
 
 export default function App() {
   return <Home />;
 }
+
+export const metadata: Metadata = {
+  title: 'Auto-Drive',
+  description:
+    'Store, share, and download your files securely with autonomys decentralized permanent storage.',
+};
