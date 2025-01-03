7f7100050122ca6151c44a8398e1fc3ffbf2aade
Carlos Lostao <hastro1990@gmail.com>
2025-01-03 14:42:51 +0100

    update: optimize landing for mobile

--- frontend/src/views/Home/index.tsx
+++ frontend/src/views/Home/index.tsx
@@ -7,16 +7,16 @@ import Image from 'next/image';
 
 export const Home = () => {
   return (
-    <div className='flex min-h-screen flex-col items-center justify-between bg-gradient-to-b from-backgroundLight to-backgroundDark'>
+    <div className='flex min-h-screen flex-col items-center justify-between gap-2 bg-gradient-to-b from-backgroundLight to-backgroundDark'>
       <LandingHeader />
-      <div className='mt-5 flex h-[50vh] w-[60%] flex-row items-center justify-center overflow-hidden rounded-3xl rounded-xl bg-white'>
+      <div className='mt-5 flex min-h-[50vh] w-[90%] flex-col items-center justify-center gap-4 overflow-hidden rounded-3xl rounded-xl bg-white py-8 md:w-[60%] md:flex-row md:gap-0 md:py-0'>
         <div className='flex h-full w-full flex-col items-center justify-center gap-4 rounded-3xl bg-white px-4'>
           <h1 className='margin-0 bg-gradient-to-b from-backgroundDarkest to-backgroundDark bg-clip-text text-4xl font-semibold text-transparent'>
             Auto-Drive
           </h1>
           <p className='text-gray max-w-[75%] text-center text-lg font-light'>
-            Store, share, and download your files securely with Autonomys Network
-            decentralized permanent storage.
+            Store, share, and download your files securely with Autonomys
+            Network decentralized permanent storage.
           </p>
           <SigningInButtons />
         </div>
