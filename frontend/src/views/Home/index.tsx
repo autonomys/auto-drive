f731a26f144739a5a323569d6ec8f4bad8f2e65a
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2025-01-09 17:26:00 -0500

    improve landing page

--- frontend/src/views/Home/index.tsx
+++ frontend/src/views/Home/index.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Link from 'next/link';
 import Footer from '../../components/Footer';
 import { LandingHeader } from '../../components/common/LandingHeader';
 import { SigningInButtons } from './SignInButtons';
@@ -12,7 +13,7 @@ export const Home = () => {
       <div className='mt-5 flex min-h-[50vh] w-[90%] flex-col items-center justify-center gap-4 overflow-hidden rounded-3xl rounded-xl bg-white py-8 md:w-[60%] md:flex-row md:gap-0 md:py-0'>
         <div className='flex h-full w-full flex-col items-center justify-center gap-4 rounded-3xl bg-white px-4'>
           <h1 className='margin-0 bg-gradient-to-b from-backgroundDarkest to-backgroundDark bg-clip-text text-4xl font-semibold text-transparent'>
-            Auto-Drive
+            Auto Drive
           </h1>
           <p className='text-gray max-w-[75%] text-center text-lg font-light'>
             Store, share, and download your files securely with Autonomys
@@ -28,6 +29,160 @@ export const Home = () => {
             width={420}
             height={272}
           />
+        </div>
+      </div>
+      <h2 className="text-center text-4xl font-bold text-backgroundDarker my-8">
+        Upload Once, Access Forever.
+      </h2>
+      <div className='mt-5 flex min-h-[50vh] w-[90%] flex-col items-center justify-center gap-4 overflow-hidden rounded-3xl rounded-xl bg-white py-8 md:w-[60%] md:flex-row md:gap-0 md:py-0'>
+        <div className='flex h-full w-full grow flex-col items-center justify-center gap-4 rounded-3xl bg-white px-4'>
+          <Image
+            className='w-full rounded-3xl border-2 border-gray-200'
+            src='/images/drag-drop-or-select-files-folders.png.png'
+            alt='Drag and drop files and folders'
+            width={420}
+            height={272}
+          />
+        </div>
+        <div className='flex h-full w-full flex-col items-center justify-center gap-4 rounded-3xl bg-white px-4'>
+          <h1 className='margin-0 bg-gradient-to-b from-backgroundDarkest to-backgroundDark bg-clip-text text-4xl font-semibold text-transparent'>
+            Upload Files & Folders
+          </h1>
+          <p className='text-gray max-w-[75%] text-center text-lg font-light'>
+            Upload files and folders to Autonomys Network decentralized permanent storage, by simply dragging and dropping them into the upload area. Or select files and folders from your computer.
+          </p>
+        </div>
+      </div>
+      <h2 className="text-center text-4xl font-bold text-backgroundDarker my-8">
+        Your Drive into Permanent Decentralized Storage.
+      </h2>
+      <div className='mt-5 flex min-h-[50vh] w-[90%] flex-col items-center justify-center gap-4 overflow-hidden rounded-3xl rounded-xl bg-white py-8 md:w-[60%] md:flex-row md:gap-0 md:py-0'>
+        <div className='flex h-full w-full flex-col items-center justify-center gap-4 rounded-3xl bg-white px-4'>
+          <h1 className='margin-0 bg-gradient-to-b from-backgroundDarkest to-backgroundDark bg-clip-text text-4xl font-semibold text-transparent'>
+            Secure End-to-End Encryption (E2EE)
+          </h1>
+          <p className='text-gray max-w-[75%] text-center text-lg font-light leading-relaxed'>
+            Secure your data with optional end-to-end encryption on Autonomys Network. Choose between setting a global encryption key for all files, customizing keys per file, or uploading without encryption - putting you in complete control of your data security.
+          </p>
+        </div>
+        <div className='flex h-full w-full grow flex-col items-center justify-center gap-4 rounded-3xl bg-white px-4'>
+          <Image
+            className='w-full rounded-3xl border-2 border-gray-200'
+            src='/images/upload-with-encryption-or-without.png'
+            alt='Upload with encryption or without'
+            width={420}
+            height={272}
+          />
+        </div>
+      </div>
+      <h2 className="text-center text-4xl font-bold text-backgroundDarker my-8">
+        Seamless Integration & Developer-Friendly
+      </h2>
+      <div className='mt-5 flex min-h-[50vh] w-[90%] flex-col items-center justify-center gap-4 overflow-hidden rounded-3xl rounded-xl bg-white py-8 md:w-[60%] md:flex-row md:gap-0 md:py-0'>
+        <div className='flex h-full w-full grow flex-col items-center justify-center gap-4 rounded-3xl bg-white px-4'>
+          <Image
+            className='w-full rounded-3xl border-2 border-gray-200'
+            src='/images/api-support.png'
+            alt='API Support'
+            width={420}
+            height={272}
+          />
+        </div>
+        <div className='flex h-full w-full flex-col items-center justify-center gap-4 rounded-3xl bg-white px-4'>
+          <h1 className='margin-0 bg-gradient-to-b from-backgroundDarkest to-backgroundDark bg-clip-text text-4xl font-semibold text-transparent'>
+            Create API Keys
+          </h1>
+          <p className='text-gray max-w-[75%] text-center text-lg font-light'>
+            Create API keys to access Autonomys Network decentralized permanent storage, through our API.
+          </p>
+        </div>
+      </div>
+      <h2 className="text-center text-4xl font-bold text-backgroundDarker my-8">
+        TypeScript & JavaScript Support with Full Type Safety
+      </h2>
+      <div className='mt-5 flex min-h-[50vh] w-[90%] flex-col items-center justify-center gap-4 overflow-hidden rounded-3xl rounded-xl bg-white py-8 md:w-[60%] md:flex-row md:gap-0 md:py-0'>
+        <div className='flex h-full w-full flex-col items-center justify-center gap-4 rounded-3xl bg-white px-4'>
+          <h1 className='margin-0 bg-gradient-to-b from-backgroundDarkest to-backgroundDark bg-clip-text text-4xl font-semibold text-transparent'>
+            @autonomys/auto-drive
+          </h1>
+          <p className='text-gray max-w-[75%] text-center text-lg font-light'>
+            Implement Auto-Drive&apos;s powerful permanent decentralized storage capabilities into your own applications using our official NPM package. Get started quickly with a familiar JavaScript/TypeScript interface and comprehensive documentation.
+            <br />
+            <br />
+            <Link
+              href='https://www.npmjs.com/package/@autonomys/auto-drive'
+              className='text-accent'
+              target='_blank'
+              rel='noreferrer'
+            >
+              npm install auto-drive
+            </Link>
+            <br />
+            <Link
+              href='https://www.npmjs.com/package/@autonomys/auto-drive'
+              className='text-accent'
+              target='_blank'
+              rel='noreferrer'
+            >
+              yarn add auto-drive
+            </Link>
+          </p>
+        </div>
+        <div className='flex h-full w-full grow flex-col items-center justify-center gap-4 rounded-3xl bg-white px-4'>
+          <Link
+            href='https://www.npmjs.com/package/@autonomys/auto-drive'
+            className='text-accent'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <Image
+              className='w-full rounded-3xl border-2 border-gray-200'
+              src='/images/auto-drive-npm.png'
+              alt='Auto-Drive NPM Package'
+              width={420}
+              height={272}
+            />
+          </Link>
+        </div>
+      </div>
+      <h2 className="text-center text-4xl font-bold text-backgroundDarker my-8">
+        Scalable Data Structure for Decentralized Storage
+      </h2>
+      <div className='mt-5 flex min-h-[50vh] w-[90%] flex-col items-center justify-center gap-4 overflow-hidden rounded-3xl rounded-xl bg-white py-8 md:w-[60%] md:flex-row md:gap-0 md:py-0'>
+        <div className='flex h-full w-full grow flex-col items-center justify-center gap-4 rounded-3xl bg-white px-4'>
+          <Link
+            href='https://www.npmjs.com/package/@autonomys/auto-dag-data'
+            className='text-accent'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <Image
+              className='w-full rounded-3xl border-2 border-gray-200'
+              src='/images/auto-dag-data.png'
+              alt='Auto-DAG Data Structure'
+              width={420}
+              height={272}
+            />
+          </Link>
+        </div>
+        <div className='flex h-full w-full flex-col items-center justify-center gap-4 rounded-3xl bg-white px-4'>
+          <h1 className='margin-0 bg-gradient-to-b from-backgroundDarkest to-backgroundDark bg-clip-text text-4xl font-semibold text-transparent'>
+            Auto-DAG Data Structure
+          </h1>
+          <p className='text-gray max-w-[75%] text-center text-lg font-light leading-relaxed'>
+            Autonomys Network uses the Auto-DAG data structure, which store your data on chain in small chunks to fit the block size limit of Autonomys Network and to ensure the integrity and authenticity of your data.
+          </p>
+        </div>
+      </div>
+      <h2 className="text-center text-4xl font-bold text-backgroundDarker my-8">
+        Join Us Today and Experience the Future of Storage!
+      </h2>
+      <div className='mt-5 flex min-h-[20vh] w-[90%] flex-col items-center justify-center gap-4 overflow-hidden rounded-3xl rounded-xl bg-white py-8 md:w-[60%] md:flex-row md:gap-0 md:py-0'>
+        <div className='flex h-full w-full flex-col items-center justify-center gap-4 rounded-3xl bg-white px-4'>
+          <p className="text-center text-lg font-light text-gray-700 mb-8">
+            Sign in now to start your journey with Autonomys Network and enjoy secure, decentralized storage.
+          </p>
+          <SigningInButtons />
         </div>
       </div>
       <Footer />
