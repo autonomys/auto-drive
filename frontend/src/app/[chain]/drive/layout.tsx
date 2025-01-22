87fe2e98ac5f94a9e9e382bad28fff97819fd017
Carlos Lostao <hastro1990@gmail.com>
2025-01-22 19:21:59 +0100

    feat: add chain dropdown

--- frontend/src/app/[chain]/drive/layout.tsx
+++ frontend/src/app/[chain]/drive/layout.tsx
@@ -18,7 +18,6 @@ import { useMemo } from 'react';
 import Image from 'next/image';
 import { useUserStore } from '../../../states/user';
 import { SessionProvider } from 'next-auth/react';
-import { Listbox } from '@headlessui/react';
 import {
   defaultNetworkId,
   NetworkId,
@@ -26,6 +25,7 @@ import {
 } from '../../../constants/networks';
 import { NetworkProvider } from '../../../contexts/network';
 import { redirect } from 'next/navigation';
+import { NetworkDropdown } from '../../../components/NetworkDropdown';
 
 export default function AppLayout({
   children,
@@ -72,15 +72,22 @@ export default function AppLayout({
     <div className='min-h-screen bg-white'>
       <div className='flex h-screen flex-col rounded-lg bg-white'>
         <header className='mb-8 flex w-full flex-col items-center justify-between gap-4 border-b-[0.2px] border-[#000000] px-16 py-2 md:flex-row md:gap-0'>
-          <div className='flex items-center space-x-2'>
-            <Image
-              src='/autonomys.png'
-              alt='Auto Drive'
-              width={16}
-              height={16}
+          <div className='flex w-full items-center justify-between space-x-2'>
+            <div className='flex items-center space-x-2'>
+              <Image
+                src='/autonomys.png'
+                alt='Auto Drive'
+                width={16}
+                height={16}
+              />
+              <span className='text-md font-medium'>Auto Drive</span>
+            </div>
+            <NetworkDropdown
+              selected={network}
+              onChange={(value) => {
+                window.location.assign(`/${value.id}/drive`);
+              }}
             />
-            <span className='text-md font-medium'>Auto Drive</span>
-            <Listbox></Listbox>
           </div>
         </header>
         <div className='flex flex-1 overflow-hidden px-10'>
