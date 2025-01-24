18f604891f789400ea1325a4cbf48eff86489027
Carlos Lostao <hastro1990@gmail.com>
2025-01-24 14:16:01 +0100

    update: backend pg driver for rds

--- backend/src/drivers/pg.ts
+++ backend/src/drivers/pg.ts
@@ -1,16 +1,41 @@
 import pg from 'pg'
-import { config } from '../config.js'
+import { config } from '../config'
+import { env } from '../utils/misc'
 
 let db: pg.Client | undefined
 
-const createDB = async (): Promise<pg.Client> => {
+export const createPgDB = async (
+  dbConfig: string | pg.ClientConfig = config.postgres.url,
+): Promise<pg.Client> => {
+  const client = new pg.Client(dbConfig)
+
+  await client.connect()
+
+  return client
+}
+
+export const createRDSConnection = async (): Promise<pg.Client> => {
   const client = new pg.Client({
-    connectionString: config.postgres.url,
+    host: env('RDS_CLUSTER_ENDPOINT'),
+    port: Number(env('RDS_CLUSTER_PORT', '5432')),
+    user: env('RDS_CLUSTER_USER', 'admin'),
+    password: env('RDS_CLUSTER_PASSWORD'),
+    database: env('DB_NAME', 'postgres'),
+    ssl: { rejectUnauthorized: false },
   })
 
   await client.connect()
 
   return client
+}
+
+export const createDB = async () => {
+  const isRDS = !!process.env.RDS_CLUSTER_ENDPOINT
+  if (isRDS) {
+    return createRDSConnection()
+  }
+
+  return createPgDB()
 }
 
 export const getDatabase = async () => {
