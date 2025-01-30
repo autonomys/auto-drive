cd157b4d71d36f4f5ec80c65c518052b814e674c
Carlos Lostao <hastro1990@gmail.com>
2025-01-30 15:54:13 +0100

    update: add publicId to hasura metadata

--- auth/src/models/jwt.ts
+++ auth/src/models/jwt.ts
@@ -1,26 +1,27 @@
 type BaseTokenPayload = {
-  id: string
-  isRefreshToken: boolean
-  oauthProvider: string
-  oauthUserId: string
-}
+  id: string;
+  isRefreshToken: boolean;
+  oauthProvider: string;
+  oauthUserId: string;
+};
 
 export type CustomAccessTokenPayload = BaseTokenPayload & {
-  isRefreshToken: false
-  refreshTokenId: string
-  'https://hasura.io/jwt/claims': {
-    'x-hasura-default-role': string
-    'x-hasura-allowed-roles': string[]
-    'x-hasura-oauth-provider': string
-    'x-hasura-oauth-user-id': string
-    'x-hasura-organization-id': string
-  }
-}
+  isRefreshToken: false;
+  refreshTokenId: string;
+  "https://hasura.io/jwt/claims": {
+    "x-hasura-default-role": string;
+    "x-hasura-allowed-roles": string[];
+    "x-hasura-oauth-provider": string;
+    "x-hasura-oauth-user-id": string;
+    "x-hasura-organization-id": string;
+    "x-hasura-public-id": string;
+  };
+};
 
 export type CustomRefreshTokenPayload = BaseTokenPayload & {
-  isRefreshToken: true
-}
+  isRefreshToken: true;
+};
 
 export type CustomTokenPayload =
   | CustomAccessTokenPayload
-  | CustomRefreshTokenPayload
+  | CustomRefreshTokenPayload;
