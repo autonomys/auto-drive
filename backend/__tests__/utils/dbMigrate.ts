dbcb7a753925cd32bb83c71c25ef1f1f3efc123f
Carlos Lostao <hastro1990@gmail.com>
2025-02-04 17:09:57 +0100

    chore: mock publish method during tests

--- backend/__tests__/utils/dbMigrate.ts
+++ backend/__tests__/utils/dbMigrate.ts
@@ -1,6 +1,7 @@
 import { closeDatabase, getDatabase } from '../../src/drivers/pg'
 import dbMigrate from 'db-migrate'
 import { Rabbit } from '../../src/drivers/rabbit'
+import { jest } from '@jest/globals'
 
 let dbMigrateInstance: ReturnType<typeof dbMigrate.getInstance>
 
@@ -9,6 +10,7 @@ const up = async () => {
   dbMigrateInstance = dbMigrate.getInstance(true)
   dbMigrateInstance.silence(true)
   await dbMigrateInstance.up()
+  jest.spyOn(Rabbit, 'publish').mockImplementation(() => Promise.resolve())
 }
 
 const down = async () => {
@@ -16,6 +18,7 @@ const down = async () => {
   await closeDatabase()
   await Rabbit.close()
   await dbMigrateInstance.down()
+  jest.restoreAllMocks()
 }
 
 export const dbMigration = {
