9ebb3beb24f72b8e714ac919178d741eb1b1b10d
Carlos Lostao <hastro1990@gmail.com>
2025-02-04 12:14:37 +0100

    update(test): add delay to teardown

--- backend/global-teardown.ts
+++ backend/global-teardown.ts
@@ -1,9 +1,11 @@
 export default async () => {
   if (global.__POSTGRES_CONTAINER__ || global.__RABBITMQ_CONTAINER__) {
     await new Promise((resolve) => {
-      global.__POSTGRES_CONTAINER__.stop()
-      global.__RABBITMQ_CONTAINER__.stop()
-      resolve(true)
+      setTimeout(() => {
+        global.__POSTGRES_CONTAINER__.stop()
+        global.__RABBITMQ_CONTAINER__.stop()
+        resolve(true)
+      }, 2_000)
     })
   } else {
     throw new Error('No database or rabbitmq container found')
