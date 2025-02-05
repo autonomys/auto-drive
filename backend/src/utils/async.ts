8d6876b4c5874cb4da85a7e65044110abfc63f2d
Carlos Lostao <hastro1990@gmail.com>
2025-02-05 16:38:32 +0100

    chore: unwrap async iterable consumer

--- backend/src/utils/async.ts
+++ backend/src/utils/async.ts
@@ -84,14 +84,12 @@ export async function forkAsyncIterable(
   const passThrough1 = new PassThrough()
   const passThrough2 = new PassThrough()
 
-  ;(async () => {
-    for await (const chunk of asyncIterable) {
-      passThrough1.write(chunk)
-      passThrough2.write(chunk)
-    }
-    passThrough1.end()
-    passThrough2.end()
-  })()
+  for await (const chunk of asyncIterable) {
+    passThrough1.write(chunk)
+    passThrough2.write(chunk)
+  }
+  passThrough1.end()
+  passThrough2.end()
 
   return [passThrough1, passThrough2]
 }
