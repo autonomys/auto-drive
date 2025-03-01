d2651576b050c634973737d5dae0d97e674f8701
Carlos Lostao <hastro1990@gmail.com>
2025-03-01 08:34:44 -0700

    fix: backend tests imports

--- backend/__tests__/e2e/uploads/files.spec.ts
+++ backend/__tests__/e2e/uploads/files.spec.ts
@@ -1,4 +1,3 @@
-import { UserWithOrganization } from '../../../src/models/users/index.js'
 import { dbMigration } from '../../utils/dbMigrate.js'
 import {
   createMockUser,
@@ -7,10 +6,11 @@ import {
 } from '../../utils/mocks.js'
 import { UploadsUseCases } from '../../../src/useCases/uploads/uploads.js'
 import {
+  UserWithOrganization,
   Upload,
   UploadStatus,
   UploadType,
-} from '../../../src/models/uploads/upload.js'
+} from '@auto-drive/models'
 import { blockstoreRepository } from '../../../src/repositories/uploads/index.js'
 import { MemoryBlockstore } from 'blockstore-core'
 import {
@@ -34,12 +34,12 @@ import {
   metadataRepository,
   nodesRepository,
 } from '../../../src/repositories/index.js'
-import { InteractionType } from '../../../src/models/objects/interactions.js'
-import { memoryDownloadCache } from '../../../src/services/download/memoryDownloadCache/index.js'
 import {
+  InteractionType,
   OwnerRole,
   TransactionStatus,
-} from '../../../src/models/objects/index.js'
+} from '@auto-drive/models'
+import { memoryDownloadCache } from '../../../src/services/download/memoryDownloadCache/index.js'
 import { FileGateway } from '../../../src/services/dsn/fileGateway/index.js'
 import { jest } from '@jest/globals'
 import { downloadService } from '../../../src/services/download/index.js'
