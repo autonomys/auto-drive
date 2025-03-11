e1d7c5a4d51355ffaf3010c6380aff57cbb296dd
Carlos Lostao <hastro1990@gmail.com>
2025-03-11 17:51:37 -0400

    feat(auth): add support for web3 JWTs

---
+++ auth/src/services/authManager/providers/web3.ts
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken'
+import { SiweMessage } from 'siwe'
+
+interface Web3AuthToken {
+  address: string
+  message: string
+  signature: string
+}
+
+export const getUserFromAccessToken = async (accessToken: string) => {
+  const decoded = jwt.decode(accessToken)
+  if (!decoded) throw new Error('Invalid token')
+  if (typeof decoded !== 'object') throw new Error('Invalid token')
+
+  const { address, message, signature } = decoded as Web3AuthToken
+
+  const siweMessage = new SiweMessage(message)
+  const { success, error } = await siweMessage.verify({
+    signature,
+    nonce: siweMessage.nonce,
+  })
+
+  if (!success) throw new Error(`Invalid signature: ${error}`)
+
+  return {
+    provider: 'auto-evm',
+    id: address,
+  }
+}
+
+export const Web3Auth = {
+  getUserFromAccessToken,
+}
