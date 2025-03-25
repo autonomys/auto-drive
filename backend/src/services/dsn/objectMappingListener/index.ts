11595e6133d67985dba4725cb69826b63dae5d98
Carlos Lostao <hastro1990@gmail.com>
2025-03-25 11:26:24 -0700

    update: rpc client to pkg

--- backend/src/services/dsn/objectMappingListener/index.ts
+++ backend/src/services/dsn/objectMappingListener/index.ts
@@ -1,34 +1,22 @@
 import { z } from 'zod'
-import { createWS } from '../../../drivers/ws.js'
 import { ObjectMappingListEntrySchema } from '@auto-drive/models'
 import { NodesUseCases } from '../../../useCases/index.js'
 import { config } from '../../../config.js'
 import { logger } from '../../../drivers/logger.js'
 import { nodesRepository } from '../../../repositories/index.js'
+import { createRpcClient } from '@autonomys/rpc'
+
+type RpcClient = ReturnType<typeof createRpcClient>
 
 const start = async () => {
-  const ws = createWS(config.objectMappingArchiver.url)
-
-  const SAFE_BLOCK_NUMBER_THRESHOLD = 100
-  const node = await nodesRepository.getFirstNotArchivedNode()
-
-  if (!node?.block_published_on) {
-    logger.info('Subscribing to real time object mappings')
-    await ws.send({
-      jsonrpc: '2.0',
-      method: 'subscribe_object_mappings',
-    })
-  } else {
-    const blockNumber = node.block_published_on
-    logger.info(`Subscribing to recover object mappings from ${blockNumber}`)
-    await ws.send({
-      jsonrpc: '2.0',
-      method: 'subscribe_recover_object_mappings',
-      params: {
-        blockNumber: blockNumber - SAFE_BLOCK_NUMBER_THRESHOLD,
-      },
-    })
-  }
+  const ws = createRpcClient({
+    endpoint: config.objectMappingArchiver.url,
+    reconnectInterval: 1_000,
+    callbacks: {
+      onReconnection: () => initSubscription(ws),
+      onOpen: () => initSubscription(ws),
+    },
+  })
 
   ws.on(async (message) => {
     const { data, success } = z
@@ -50,6 +38,29 @@ const start = async () => {
       await NodesUseCases.scheduleNodeArchiving(data.result.v0.objects)
     }
   })
+}
+
+const initSubscription = async (ws: RpcClient) => {
+  const SAFE_BLOCK_NUMBER_THRESHOLD = 100
+  const node = await nodesRepository.getFirstNotArchivedNode()
+
+  if (!node?.block_published_on) {
+    logger.info('Subscribing to real time object mappings')
+    await ws.send({
+      jsonrpc: '2.0',
+      method: 'subscribe_object_mappings',
+    })
+  } else {
+    const blockNumber = node.block_published_on
+    logger.info(`Subscribing to recover object mappings from ${blockNumber}`)
+    await ws.send({
+      jsonrpc: '2.0',
+      method: 'subscribe_recover_object_mappings',
+      params: {
+        blockNumber: blockNumber - SAFE_BLOCK_NUMBER_THRESHOLD,
+      },
+    })
+  }
 }
 
 export const objectMappingArchiver = {
