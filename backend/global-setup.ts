35c7ff43234b87b6f28f4871bd0a1a19fd79c279
Carlos Lostao <hastro1990@gmail.com>
2025-06-03 20:44:39 +0200

    chore: update PostgreSQL and RabbitMQ container images in global setup

--- backend/global-setup.ts
+++ backend/global-setup.ts
@@ -5,12 +5,9 @@ import fsPromise from 'fs/promises'
 
 export default async () => {
   config({ path: '.env.test' })
-  const postgresContainer = new PostgreSqlContainer().withExposedPorts(54321)
+  const postgresContainer = new PostgreSqlContainer('postgres:16')
   const service = await postgresContainer.start()
-  const rabbitmqContainer = new RabbitMQContainer().withExposedPorts(
-    5672,
-    15672,
-  )
+  const rabbitmqContainer = new RabbitMQContainer('rabbitmq:3.12')
   const rabbitmqService = await rabbitmqContainer.start()
 
   // Wiping the cache directory for not having any cache from previous tests
