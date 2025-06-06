f3a5aebbf0ec529ffd22cf5a6e1b39275078e2ad
Carlos Lostao <hastro1990@gmail.com>
2025-06-06 14:56:03 +0200

    fix: missing object mapping archiver

--- backend/src/servers/frontendWorker.ts
+++ backend/src/servers/frontendWorker.ts
@@ -1,3 +1,4 @@
+import { logger } from '../drivers/logger.js'
 ;(async () => {
   if (process.env.NODE_ENV === 'production') {
     await import('./awsSetup.js').then(({ setupFinished }) => setupFinished)
@@ -5,5 +6,22 @@
 
   await import('../apis/worker.js')
   const { EventRouter } = await import('../services/eventRouter/index.js')
-  EventRouter.listenFrontendEvents()
+  const { config } = await import('../config.js')
+  const { objectMappingArchiver } = await import(
+    '../services/dsn/objectMappingListener/index.js'
+  )
+
+  if (config.services.taskManager.active) {
+    EventRouter.listenFrontendEvents()
+  }
+  if (config.services.objectMappingArchiver.active) {
+    objectMappingArchiver.start()
+  }
+  if (
+    !config.services.taskManager.active &&
+    !config.services.objectMappingArchiver.active
+  ) {
+    logger.info('No services active, exiting')
+    process.exit(1)
+  }
 })()
