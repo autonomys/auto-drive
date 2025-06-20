f207247d8f3aa1ad573e68fa0bac73edab590b2d
Carlos Lostao <hastro1990@gmail.com>
2025-06-20 17:23:23 +0200

    refactor: simplify index drop statements in migration SQL file

--- backend/migrations/sqls/20250620113124-indexes-down.sql
+++ backend/migrations/sqls/20250620113124-indexes-down.sql
@@ -1,15 +1,12 @@
--- users_organizations
-DROP INDEX IF EXISTS public.idx_users_organizations_organization_id;
-
 -- interactions
-DROP INDEX IF EXISTS public.idx_interactions_subscription_id;
+DROP INDEX IF EXISTS idx_interactions_subscription_id;
 
 -- nodes
-DROP INDEX IF EXISTS public.idx_nodes_root_cid;
-DROP INDEX IF EXISTS public.idx_nodes_head_cid;
+DROP INDEX IF EXISTS idx_nodes_root_cid;
+DROP INDEX IF EXISTS idx_nodes_head_cid;
 
 -- published_objects
-DROP INDEX IF EXISTS public.idx_published_objects_cid;
+DROP INDEX IF EXISTS idx_published_objects_cid;
 
 -- metadata
-DROP INDEX IF EXISTS public.idx_metadata_head_cid;
+DROP INDEX IF EXISTS idx_metadata_head_cid;
