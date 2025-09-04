db9d66e45c3e64bc987be25d8f0da3cd33870b6d
clostao <49268919+clostao@users.noreply.github.com>
2025-09-04 14:44:59 +0200

    Fix anonymous metadata query (#468)
    * refactor: create an apps folder that includes all services
    
    * feat: add UI package with atomic components and utilities
    
    * feat: initialize landing page with Next.js, Tailwind CSS, and essential configurations
    
    - Add ESLint and Prettier configurations for code quality.
    - Create Next.js configuration for custom webpack settings.
    - Set up Tailwind CSS with a comprehensive theme and utility classes.
    - Implement landing page layout with header, footer, and main sections.
    - Include components for user interaction, such as sign-in options and upload functionality.
    - Establish a responsive design with a focus on user experience and accessibility.
    - Add README for project setup and usage instructions.
    
    * feat: integrate @auto-drive/ui package and refactor components
    
    - Add @auto-drive/ui as a dependency for shared components and utilities.
    - Update Tailwind configuration to include UI package paths for content.
    - Refactor various components to utilize the new UI package for buttons and routes.
    - Remove deprecated components and files, streamlining the codebase.
    - Adjust navigation logic in the app to enhance user experience.
    
    * chore: add @auto-drive/ui as a workspace dependency in yarn.lock
    
    * chore: add UI build step to Makefile and include it in common targets
    
    * fix: some old imports
    
    * chore: add landing and UI workspace commands to package.json
    
    * chore: update Makefile to include 'ui' in the all target
    
    * chore: update test and build commands in package.json for auth and backend apps
    
    * feat: implement automatic login and authentication redirection
    
    * fix: update redirection path in page component from explorer to drive/global
    
    * refactor: remove unused imports in SideNavBar component
    
    * refactor: replace useAutomaticLogin hook with AutomaticLoginWrapper component in AppLayout
    
    * fix: anonymous fetch metadata query by head_cid
    
    * feat: implements an error page if some error happen in server side

--- apps/frontend/src/app/[chain]/drive/metadata/[cid]/page.tsx
+++ apps/frontend/src/app/[chain]/drive/metadata/[cid]/page.tsx
@@ -1,27 +1,48 @@
 import {
   GetMetadataByHeadCidDocument,
   GetMetadataByHeadCidQuery,
+  GetMetadataByHeadCidWithOwnershipDocument,
+  GetMetadataByHeadCidWithOwnershipQuery,
 } from 'gql/graphql';
 import { ObjectDetails } from '@/components/organisms/ObjectDetails';
-import { mapObjectInformationFromQueryResult } from 'services/gql/utils';
+import {
+  mapObjectInformationFromQueryResult,
+  mapObjectInformationFromQueryResultWithOwnership,
+} from 'services/gql/utils';
 import { NetworkId } from '@auto-drive/ui';
 import { createGQLClientByNetwork } from 'services/gql';
+import { getServerSession } from 'next-auth';
 
 export const dynamic = 'force-dynamic';
+
+const getMetadata = async (cid: string, chain: NetworkId) => {
+  const gqlClient = createGQLClientByNetwork(chain);
+  const session = await getServerSession();
+
+  if (session) {
+    const { data } =
+      await gqlClient.query<GetMetadataByHeadCidWithOwnershipQuery>({
+        query: GetMetadataByHeadCidWithOwnershipDocument,
+        variables: { headCid: cid },
+      });
+
+    return mapObjectInformationFromQueryResultWithOwnership(data);
+  } else {
+    const { data } = await gqlClient.query<GetMetadataByHeadCidQuery>({
+      query: GetMetadataByHeadCidDocument,
+      variables: { headCid: cid },
+    });
+
+    return mapObjectInformationFromQueryResult(data);
+  }
+};
 
 export default async function Page({
   params: { cid, chain },
 }: {
   params: { cid: string; chain: NetworkId };
 }) {
-  const gqlClient = createGQLClientByNetwork(chain);
-
-  const { data } = await gqlClient.query<GetMetadataByHeadCidQuery>({
-    query: GetMetadataByHeadCidDocument,
-    variables: { headCid: cid },
-  });
-
-  const metadata = mapObjectInformationFromQueryResult(data);
+  const metadata = await getMetadata(cid, chain);
 
   return <ObjectDetails object={metadata} />;
 }
--- apps/frontend/src/services/gql/utils.ts
+++ apps/frontend/src/services/gql/utils.ts
@@ -1,4 +1,7 @@
-import { GetMetadataByHeadCidQuery } from 'gql/graphql';
+import {
+  GetMetadataByHeadCidQuery,
+  GetMetadataByHeadCidWithOwnershipQuery,
+} from 'gql/graphql';
 import { ObjectInformation, OwnerRole, objectStatus } from '@auto-drive/models';
 
 export const mapObjectInformationFromQueryResult = (
@@ -26,7 +29,37 @@ export const mapObjectInformationFromQueryResult = (
     status: objectStatus(uploadState),
     uploadState,
     tags: metadata.tags ?? [],
-    owners: metadata.object_ownership.map((owner) => ({
+    owners: [],
+    publishedObjectId: null,
+  };
+};
+
+export const mapObjectInformationFromQueryResultWithOwnership = (
+  result: GetMetadataByHeadCidWithOwnershipQuery,
+): ObjectInformation => {
+  const metadata = result.metadata.at(0);
+  if (!metadata) {
+    throw new Error('Metadata not found');
+  }
+
+  const uploadState = {
+    uploadedNodes: metadata.publishedNodes?.aggregate?.count ?? 0,
+    totalNodes: metadata.totalNodes?.aggregate?.count ?? 0,
+    archivedNodes: metadata.archivedNodes?.aggregate?.count ?? 0,
+    minimumBlockDepth:
+      metadata.minimumBlockDepth?.[0]?.block_published_on ?? null,
+    maximumBlockDepth:
+      metadata.maximumBlockDepth?.[0]?.block_published_on ?? null,
+  };
+
+  return {
+    cid: metadata.head_cid,
+    metadata: metadata.metadata,
+    createdAt: metadata.created_at,
+    status: objectStatus(uploadState),
+    uploadState,
+    tags: metadata.tags ?? [],
+    owners: metadata.object_ownership?.map((owner) => ({
       role: owner.is_admin ? OwnerRole.ADMIN : OwnerRole.VIEWER,
       oauthProvider: owner.oauth_provider ?? '',
       oauthUserId: owner.oauth_user_id ?? '',
