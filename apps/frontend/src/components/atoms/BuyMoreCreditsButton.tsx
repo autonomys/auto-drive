b94374f8fa0f5d115a69f5100b2d249885fb7dc5
Carlos Lostao <hastro1990@gmail.com>
2025-09-30 14:39:01 +0200

    Add BuyMoreCreditsButton component for purchasing credits with network context integration

--- apps/frontend/src/components/atoms/BuyMoreCreditsButton.tsx
+++ apps/frontend/src/components/atoms/BuyMoreCreditsButton.tsx
@@ -4,7 +4,7 @@ import { useNetwork } from '../../contexts/network';
 
 export const BuyMoreCreditsButton = () => {
   const { network } = useNetwork();
-  
+
   return (
     <InternalLink href={ROUTES.purchase(network.id)} className='contents'>
       <Button variant='outline' size='sm' className='w-full text-xs'>
